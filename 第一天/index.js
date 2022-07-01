dd1eeaf26f46c969d452cff63cb73f0e27db25de
iamzjt-front-end <iamzjt@foxmail.com>
2022-07-01 17:48:47 +0800

    练习

--- 第一天/index.js
+++ 第一天/index.js
@@ -1,2 +1,57 @@
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+
+// const aa = 11;
+// const bb = 22;
+// const cc = 33;
+// const dd = 44;
+
+// const aa = 11;
+// const bb = 22;
+// const cc = 33;
+// const dd = 44;
+
+// const aa = 11;
+// const bb = 22;
+// const cc = 33;
+// const dd = 44;
+
+// const aa = 11;
+// const bb = 22;
+// const cc = 33;
+// const dd = 44;
+
 const aa = 11;
 const bb = 22;
+const cc = 33;
+const dd = 44;
