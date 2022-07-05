23e87c2c6d47d69b407a6e845b5a3772fb9e7fa9
iamzjt-front-end <iamzjt@foxmail.com>
2022-07-05 10:50:00 +0800

    vim03

---
+++ 第三天/index.js
@@ -0,0 +1,13 @@
+// 你不知道的JavaScript
+function foo(el) {
+  console.log(el, this.id);
+}
+
+let obj = {
+  id: 'awesome',
+}
+
+let nums = [1, 2, 3];
+nums.forEach(foo,obj);
+
+// you're keyboard man
