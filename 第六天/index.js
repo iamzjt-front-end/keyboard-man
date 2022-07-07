1a947e3ce4e957d8c3083a6f564295153372900a
iamzjt-front-end <iamzjt@foxmail.com>
2022-07-07 13:13:13 +0800

    第六天

---
+++ 第六天/index.js
@@ -0,0 +1,16 @@
+// 你不知道的JavaScript
+function foo(el) {
+  console.log(el, this.id);
+}
+
+let obj = {
+  id: 'awesome',
+}
+
+let nums = [1, 2, 3];
+nums.forEach((item, index) => {
+  console.log('111' + item); 
+  console.log('222' + item); 
+  console.log('333' + item); 
+  console.log('555' + item); 
+});
