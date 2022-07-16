bfbbd5d50f62ce24ddc9349405d8ae9894095102
iamzjt-front-end <iamzjt@foxmail.com>
2022-07-16 11:23:55 +0800

    vim10

---
+++ 第十天/index.js
@@ -0,0 +1,26 @@
+// 你不知道的JavaScript
+function foo(el) {
+  console.log(el, this.id);
+}
+
+let obj = {
+  id: "awesome",
+};
+
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+// let hahaha = [1, 2, 3];
+nums.forEach(foo, obj);
